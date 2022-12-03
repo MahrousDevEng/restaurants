0942447a84f6f39e77972b0f3278880685fe7a82
Mohamed Mahrous <eng.mahrous17@gmail.com>
2022-12-04 00:49:55 +0200

    upload home page

---
+++ js/home.js
@@ -0,0 +1,41 @@
+$(function () {
+  "use strict";
+
+  // Trigger LazyLoad
+  const lazyLoadInstance = new LazyLoad();
+
+  // Trigger Swiper
+  const swiperWrapper = $("#homeSwiper");
+  const swiperSlides = $("#homeSwiper .swiper-slide");
+
+  if (swiperWrapper.length !== 0) {
+    const swiper = new Swiper("#homeSwiper", {
+      effect: "coverflow",
+      coverflowEffect: {
+        rotate: 50,
+        depth: 150,
+      },
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      autoplay: {
+        delay: 5000,
+      },
+      breakpoints: {
+        768: {
+          autoplay: false,
+        },
+      },
+      on: {
+        init: (sw) => handleChangeTheme(sw),
+        // Handle Slide Change and Change Theme
+        slideChange: (sw) => handleChangeTheme(sw),
+      },
+    });
+  }
+
+  function handleChangeTheme(sw) {
+    const currentSlideTheme = swiperSlides?.eq(sw.activeIndex)?.data("theme");
+    $("body").attr("data-current-theme", currentSlideTheme);
+  }
+});
