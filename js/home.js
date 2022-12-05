c28d69827af0bd042e0d6087e6d541197b64980b
Mohamed Mahrous <eng.mahrous17@gmail.com>
2022-12-05 23:10:41 +0200

    finish home

--- js/home.js
+++ js/home.js
@@ -5,11 +5,13 @@ $(function () {
   const lazyLoadInstance = new LazyLoad();
 
   // Trigger Swiper
-  const swiperWrapper = $("#homeSwiper");
-  const swiperSlides = $("#homeSwiper .swiper-slide");
+  const swiperWrapper = $("#homeSwiper"),
+    nextBtn = $("#nextSlide"),
+    prevBtn = $("#prevSlide");
 
   if (swiperWrapper.length !== 0) {
     const swiper = new Swiper("#homeSwiper", {
+      initialSlide: 1,
       effect: "coverflow",
       coverflowEffect: {
         rotate: 50,
@@ -18,6 +20,7 @@ $(function () {
       grabCursor: true,
       centeredSlides: true,
       slidesPerView: "auto",
+      speed: 1500,
       autoplay: {
         delay: 5000,
       },
@@ -25,17 +28,15 @@ $(function () {
         768: {
           autoplay: false,
           effect: "slide",
-          centeredSlides: false,
           slidesPerView: 2.25,
           spaceBetween: 30,
         },
         1200: {
           autoplay: false,
           effect: "slide",
-          centeredSlides: false,
           slidesPerView: 3,
           spaceBetween: 50,
-        }
+        },
       },
       on: {
         init: (sw) => handleChangeTheme(sw),
@@ -43,10 +44,53 @@ $(function () {
         slideChange: (sw) => handleChangeTheme(sw),
       },
     });
+
+    // Next Slide Action
+    nextBtn.on("click", () => swiper.slideNext(1500));
+
+    // Previous Slide Action
+    prevBtn.on("click", () => swiper.slidePrev(1500));
   }
 
   function handleChangeTheme(sw) {
-    const currentSlideTheme = swiperSlides?.eq(sw.activeIndex)?.data("theme");
+    const currentSlideTheme = sw.slides[sw.activeIndex].dataset.theme;
     $("body").attr("data-current-theme", currentSlideTheme);
+
+    const selectBg = sw.slides[sw.activeIndex].dataset.image;
+    const bgWrapper = $("#pageBgWrapper");
+    bgWrapper.animate(
+      {
+        width: 0,
+        height: 0,
+      },
+      500,
+      function () {
+        bgWrapper.css("background-image", `url("${selectBg}")`);
+        bgWrapper.animate(
+          {
+            width: "100%",
+            height: "100%",
+          },
+          500
+        );
+      }
+    );
+
+    const totalSlides = sw.slides.length;
+    const activeIndex = sw.activeIndex;
+
+    if (activeIndex === totalSlides - 1) {
+      nextBtn.addClass("disabled").attr("disabled", true);
+    } else {
+      nextBtn.hasClass("disabled") &&
+        nextBtn.removeClass("disabled").attr("disabled", null);
+    }
+
+    if (activeIndex === 0) {
+      prevBtn.addClass("disabled").attr("disabled", true);
+    } else {
+      prevBtn.hasClass("disabled") &&
+        prevBtn.removeClass("disabled").attr("disabled", null);
+    }
   }
 });
